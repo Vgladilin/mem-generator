fbc0c2dd3d9ddfe663cbc80e313421f100da27d5
Vgladilin <vgladilin40@gmail.com>
2023-04-13 13:39:51 +0300

    feat: main funcs & major refactor project's struct

--- scripts/memePage.js
+++ scripts/memePage.js
@@ -1,13 +1,36 @@
 const chosenMeme = get('meme') || ''
 const maxBoxes = get('maxtextbox') || ''
-function get(name){
-    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+
+function get(name) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
         return decodeURIComponent(name[1]);
 }
 
 function setData() {
     document.querySelector('#imageName').innerText = chosenMeme
+    getImage(
+        chosenMeme,
+        document.querySelector('#imageContentBefore')
+    )
 }
+
+function createMeme() {
+    const text = document.querySelector('#inp').value
+    const data = {
+        "text0": {
+            "text": text,
+            "font_size": 33
+        }
+    }
+
+    getImage(
+        chosenMeme,
+        document.querySelector('#imageContentAfter'),
+        data
+    )
+
+}
+
 $(document).ready(function () {
-   setData()
+    setData()
 })
--- scripts/script.js
+++ scripts/script.js
@@ -276,32 +276,7 @@
 // }
 
 /* js */
-function getImage(name, img, data = {}) {
-    $.ajax({
-        "async": true,
-        "crossDomain": true,
-        "url": "https://meme-generator-and-template-database.p.rapidapi.com/template/"+name,
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "X-RapidAPI-Key": "c1d153bc9bmshae39c02725a86a7p1318dfjsn7150214728f0",
-            "X-RapidAPI-Host": "meme-generator-and-template-database.p.rapidapi.com"
-        },
-        "xhrFields": {
-            "responseType": 'blob'
-        },
-        "processData": false,
-        "data": JSON.stringify(data),
-        success: function (data) {
 
-            const url = window.URL || window.webkitURL;
-            const src = url.createObjectURL(data);
-            img.setAttribute('src', src);
-
-
-        }
-    });
-}
 function renderImagesList(data) {
     let wrapper = document.querySelector('#images')
     let counter = 0
